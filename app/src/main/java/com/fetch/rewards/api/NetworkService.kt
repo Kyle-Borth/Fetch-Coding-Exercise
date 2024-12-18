0aea6bf531cd285bb5fc37696348e08d076e01f8
Kyle-Borth <kyle.borth@outlook.com>
2024-12-18 17:38:27 -0600

    Implemented NetworkService base class

---
+++ app/src/main/java/com/fetch/rewards/api/NetworkService.kt
@@ -0,0 +1,67 @@
+package com.fetch.rewards.api
+
+import android.util.Log
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+private const val TAG = "NtwrkSrvs"
+
+abstract class NetworkService {
+
+    //TODO Move out of this class
+    protected val converterFactory by lazy {
+        jsonSerializer.asConverterFactory("application/json".toMediaType())
+    }
+
+    protected suspend fun <T> wrapRequest(
+        request: suspend () -> Response<T>
+    ): WrappedResponse<T> = withContext(Dispatchers.IO) {
+        try {
+            val response = request()
+
+            if(response.isSuccessful) {
+                response.body()?.let { body ->
+                    WrappedResponse.SuccessWithBody(body, response.code())
+                } ?: WrappedResponse.SuccessNoBody(response.code())
+            }
+            else {
+                WrappedResponse.NetworkError(response.errorBody(), null, response.code())
+            }
+        }
+        catch (ioE: IOException) {
+            // May indicate the user does not have an internet connection
+            Log.e(TAG, "API Request Exception", ioE)
+            WrappedResponse.LocalError(ioE)
+        }
+        catch (httpE: HttpException) {
+            // Error response from the API
+            Log.e(TAG, "API Request Exception: ${httpE.message()}", httpE)
+            WrappedResponse.NetworkError(null, httpE, httpE.code())
+        }
+    }
+
+    protected fun buildOkHttpClient(timeoutSeconds: Long = 10L) = OkHttpClient.Builder().apply {
+        connectTimeout(timeoutSeconds, TimeUnit.SECONDS)
+        readTimeout(timeoutSeconds, TimeUnit.SECONDS)
+        writeTimeout(timeoutSeconds, TimeUnit.SECONDS)
+    }.build()
+
+    companion object {
+        val jsonSerializer by lazy {
+            Json {
+                ignoreUnknownKeys = true
+                explicitNulls = false
+                coerceInputValues = true
+            }
+        }
+    }
+
+}
